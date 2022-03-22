d29fb9fa5e19e7d02519657b7836ba4a64a42bd7
Christian Jülg <christian.juelg@optonic.com>
2022-03-22 12:49:17 +0100

    handle controller not started yet; TODO: handle restart / timeout
    * appears to fix issues when launching during controller startup, but controller restarts are not handled
    * exit joint_trajectory_streamer if initialisation was not successful
    * add respawn_delay to avoid 100% cpu usage
    * exit motion_ctrl if connection fails

--- motoman_driver/src/joint_streaming_node.cpp
+++ motoman_driver/src/joint_streaming_node.cpp
@@ -59,7 +59,12 @@ int main(int argc, char** argv)
 
   // launch the FS100 JointTrajectoryStreamer connection/handlers
   motionInterface.reset(new MotomanJointTrajectoryStreamer());
-  motionInterface->init("", FS100_motion_port, false);
+
+  bool isOk = motionInterface->init("", FS100_motion_port, false);
+  if (!isOk) {
+    ROS_ERROR_STREAM("joint_trajectory_streamer: init failed, respawning");
+    throw std::runtime_error("respawn motoman streaming interface, controller was not initialised");
+  }
   motionInterface->run();
 
   return 0;
