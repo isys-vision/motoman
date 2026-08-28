199cf9eee0bedbfca2a44e60eac33e50822fe96c
Christian Jülg <christian.juelg@optonic.com>
2026-08-28 14:41:19 +0200

    use motoplus IO signal defines

--- motoman_driver/MotoPlus/Controller.c
+++ motoman_driver/MotoPlus/Controller.c
@@ -438,24 +438,24 @@ closeSockHandle:
 //-------------------------------------------------------------------
 void Ros_Controller_StatusInit(Controller* controller)
 {
-	controller->ioStatusAddr[IO_ROBOTSTATUS_ALARM_MAJOR].ulAddr = 50010;		// Alarm
-	controller->ioStatusAddr[IO_ROBOTSTATUS_ALARM_MINOR].ulAddr = 50011;		// Alarm
-	controller->ioStatusAddr[IO_ROBOTSTATUS_ALARM_SYSTEM].ulAddr = 50012;		// Alarm
-	controller->ioStatusAddr[IO_ROBOTSTATUS_ALARM_USER].ulAddr = 50013;			// Alarm
-	controller->ioStatusAddr[IO_ROBOTSTATUS_ERROR].ulAddr = 50014;				// Error
-	controller->ioStatusAddr[IO_ROBOTSTATUS_PLAY].ulAddr = 50054;				// Play
-	controller->ioStatusAddr[IO_ROBOTSTATUS_TEACH].ulAddr = 50053;				// Teach
-	controller->ioStatusAddr[IO_ROBOTSTATUS_REMOTE].ulAddr = 80011; //50056;	// Remote  // Modified E.M. 7/9/2013
-	controller->ioStatusAddr[IO_ROBOTSTATUS_OPERATING].ulAddr = 50070;			// Operating
-	controller->ioStatusAddr[IO_ROBOTSTATUS_HOLD].ulAddr = 50071;				// Hold
-	controller->ioStatusAddr[IO_ROBOTSTATUS_SERVO].ulAddr = 50073;   			// Servo ON
-	controller->ioStatusAddr[IO_ROBOTSTATUS_EXT_SERVO_OFF1].ulAddr = 40065;   	// EXT Servo OFF 1
-	controller->ioStatusAddr[IO_ROBOTSTATUS_ESTOP_EX].ulAddr = 80025;   		// External E-Stop
-	controller->ioStatusAddr[IO_ROBOTSTATUS_ESTOP_PP].ulAddr = 80026;   		// Pendant E-Stop
-	controller->ioStatusAddr[IO_ROBOTSTATUS_ESTOP_CTRL].ulAddr = 80027;   		// Controller E-Stop
+	controller->ioStatusAddr[IO_ROBOTSTATUS_ALARM_MAJOR].ulAddr = MP_SOUT_MAJOR_ALARM_OCCUR;	// Alarm
+	controller->ioStatusAddr[IO_ROBOTSTATUS_ALARM_MINOR].ulAddr = MP_SOUT_MINOR_ALARM_OCCUR;	// Alarm
+	controller->ioStatusAddr[IO_ROBOTSTATUS_ALARM_SYSTEM].ulAddr = MP_SOUT_SYSTEM_ALARM_OCCUR;	// Alarm
+	controller->ioStatusAddr[IO_ROBOTSTATUS_ALARM_USER].ulAddr = MP_SOUT_USER_ALARM_OCCUR;		// Alarm
+	controller->ioStatusAddr[IO_ROBOTSTATUS_ERROR].ulAddr = MP_SOUT_ERROR_OCCUR;				// Error
+	controller->ioStatusAddr[IO_ROBOTSTATUS_PLAY].ulAddr = MP_SOUT_PLAY_MODE_SET;				// Play
+	controller->ioStatusAddr[IO_ROBOTSTATUS_TEACH].ulAddr = MP_SOUT_TEACH_MODE_SET;				// Teach
+	controller->ioStatusAddr[IO_ROBOTSTATUS_REMOTE].ulAddr = 80011; //50056;					// Remote  // Modified E.M. 7/9/2013
+	controller->ioStatusAddr[IO_ROBOTSTATUS_OPERATING].ulAddr = MP_SOUT_RUNNING;				// Operating
+	controller->ioStatusAddr[IO_ROBOTSTATUS_HOLD].ulAddr = MP_SOUT_HOLDING;						// Hold
+	controller->ioStatusAddr[IO_ROBOTSTATUS_SERVO].ulAddr = MP_SOUT_SERVO_ON;   				// Servo ON
+	controller->ioStatusAddr[IO_ROBOTSTATUS_EXT_SERVO_OFF1].ulAddr = MP_SIN_EXT_SERVO_OFF_1;   	// EXT Servo OFF 1
+	controller->ioStatusAddr[IO_ROBOTSTATUS_ESTOP_EX].ulAddr = 80025;   						// External E-Stop
+	controller->ioStatusAddr[IO_ROBOTSTATUS_ESTOP_PP].ulAddr = 80026;   						// Pendant E-Stop
+	controller->ioStatusAddr[IO_ROBOTSTATUS_ESTOP_CTRL].ulAddr = 80027;   						// Controller E-Stop
 	controller->ioStatusAddr[IO_ROBOTSTATUS_WAITING_ROS].ulAddr = IO_FEEDBACK_WAITING_MP_INCMOVE; // Job input signaling ready for external motion
-	controller->ioStatusAddr[IO_ROBOTSTATUS_ROS_DONE].ulAddr = IO_FEEDBACK_MP_INCMOVE_DONE; // Job input signaling external motion done
-	controller->ioStatusAddr[IO_ROBOTSTATUS_INECOMODE].ulAddr = 50727;			// Energy Saving Mode
+	controller->ioStatusAddr[IO_ROBOTSTATUS_ROS_DONE].ulAddr = IO_FEEDBACK_MP_INCMOVE_DONE; 	// Job input signaling external motion done
+	controller->ioStatusAddr[IO_ROBOTSTATUS_INECOMODE].ulAddr = 50727;							// Energy Saving Mode
 #if (YRC1000||YRC1000u)
 	controller->ioStatusAddr[IO_ROBOTSTATUS_PFL_STOP].ulAddr = 81702;			// PFL function stopped the motion
 	controller->ioStatusAddr[IO_ROBOTSTATUS_PFL_ESCAPE].ulAddr = 81703;			// PFL function escape from clamping motion
